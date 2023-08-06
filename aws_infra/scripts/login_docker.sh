4a2f5aca0bc1dec5cedc7f95fed5998965c34e92
Andy <andy.schunke@woodmark.de>
2023-08-06 13:49:43 +0200

    debug

--- aws_infra/scripts/login_docker.sh
+++ aws_infra/scripts/login_docker.sh
@@ -2,12 +2,15 @@
 
 # This script is used to login to docker hub, username und password will be read from .env file wich is located in the root directory of this project
 echo "Login to docker hub"
-source .env
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+env_file="$script_dir/.env"
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-echo "Login to docker hub done"
-```
-
-```bash
+if [ -f "$env_file" ]; then
+   source "$env_file"
+    echo "Login to docker hub done"
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+else
+    echo "Error: .env file not found in the script directory."
+fi
 
 
