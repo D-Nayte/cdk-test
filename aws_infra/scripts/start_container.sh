d8265512f5a699b6e2db57b5c699786f04036720
Andy <andy.schunke@woodmark.de>
2023-08-06 13:28:17 +0200

    debug

--- aws_infra/scripts/start_container.sh
+++ aws_infra/scripts/start_container.sh
@@ -2,7 +2,7 @@
 
 # This script is used to login to docker hub, username und password will be read from .env file wich is located in the root directory of this project
 echo "Start container"
-source ../.env
+source .env
 
 docker run -p 80:3000 $DOCKER_IMAGE_NAME
 
